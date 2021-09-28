9a8df4fce94872226abf513410ea13c20642ec95
SHILPA <andavarapushilpa95@gmail.com>
2021-09-28 13:08:20 +0200

    added functionalities of Searchbar and Dashboard componentsalong eith localStorage

--- weather-app-dashboard/src/Components/WeatherApp/DashBoard.js
+++ weather-app-dashboard/src/Components/WeatherApp/DashBoard.js
@@ -1,34 +1,106 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
 
-const DashBoard = () => {
+const api = {
+    key: "98f50f7518cdc2d04a58254249039a9f",
+    base: "https://api.openweathermap.org/data/2.5/",
+};
+
+const DashBoard = (props) => {
+    const { history } = props;
+    const checkTiles = localStorage.getItem("tiles");
+    const initialTiles = checkTiles === null ? [] : JSON.parse(checkTiles);
+    const [tiles, setTiles] = useState(initialTiles);
+    const [coords, setCoords] = useState({});
+    const [currentLocation, setCurrentLocation] = useState({});
+
+    //to get the coordinates (latitude and longitude)
+    useEffect(() => {
+        const getLocation = () => {
+            if (navigator.geolocation) {
+                return navigator.geolocation.getCurrentPosition(showPosition);
+            }
+            return false;
+        };
+        getLocation();
+    }, []);
+
+    //To get the city name using the latitude and longitude from getCurrentPosition and set the state to that particular location
+    useEffect(() => {
+        if (Object.entries(coords).length !== 0) {
+            axios
+                .get(
+                    `${api.base}weather?lat=${coords.lat}&lon=${coords.lon}&units=metric&APPID=${api.key}`
+                )
+                .then((response) => {
+                    if (response.data) {
+                        setCurrentLocation(response.data);
+                    }
+                })
+                .catch((error) => {
+                    console.log(error.message);
+                });
+        }
+    }, [coords]);
+    //If currentLocation is not empty then Mylocation tile will be shown.
+    useEffect(() => {
+        if (Object.entries(currentLocation).length !== 0) {
+            const data = [...tiles];
+            const cityExists = data.find((x) => x.name === "My Location");
+            if (cityExists === undefined) {
+                data.push({
+                    id: currentLocation.id,
+                    name: "My Location",
+                    temp: currentLocation.main.temp,
+                });
+                setTilesData(data);
+            }
+        }
+    }, [currentLocation]);
+
+    //To set the data in localStorage.
+    const setTilesData = (data) => {
+        setTiles(data);
+        localStorage.setItem("tiles", JSON.stringify(data));
+    };
+
+    //This function sets the coordinates(latitude and longitude) to get the current position
+    const showPosition = (position) => {
+        setCoords({
+            lat: position.coords.latitude,
+            lon: position.coords.longitude,
+        });
+    };
+    //this is the function to handle the click the tile which directs to weather-details page of paticular city
+    const handleClick = (city) => {
+        history.push(`/weather-details/${city}`);
+    };
+
+    //This function creates the tile when clicked on search button
+    const createTile = () => {
+        const tileData = tiles.map((x) => {
+            return (
+                <div
+                    className="tile-wrapper"
+                    key={x.id}
+                    onClick={() => handleClick(x.name)}
+                >
+                    <label htmlFor="city-name" className="city-label">
+                        {x.name}
+                    </label>
+                    <div className="temp">{Math.round(x.temp)}°C</div>
+                </div>
+            );
+        });
+        return tileData;
+    };
     return (
         <div className="dash-board-wrapper">
-            <SearchBar />
+            <SearchBar setTiles={setTilesData} tiles={tiles} />
             <h1 className="header-dashboard">Dashboard</h1>
 
-            <div className="dash-board-container">
-                <div className="tile-wrapper">
-                    <label htmlFor="city-name" className="city-label">
-                        My Location
-                    </label>
-                    <div className="temp">17°C</div>
-                </div>
-
-                <div className="tile-wrapper">
-                    <label htmlFor="city-name" className="city-label">
-                        Berlin
-                    </label>
-                    <div className="temp">20°C</div>
-                </div>
-
-                <div className="tile-wrapper">
-                    <label htmlFor="city-name" className="city-label">
-                        London
-                    </label>
-                    <div className="temp">18°C</div>
-                </div>
-            </div>
+            <div className="dash-board-container">{createTile()}</div>
         </div>
     );
 };
--- weather-app-dashboard/src/Components/WeatherApp/SearchBar.js
+++ weather-app-dashboard/src/Components/WeatherApp/SearchBar.js
@@ -1,20 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
-const SearchBar = () => {
+const api = {
+    key: "98f50f7518cdc2d04a58254249039a9f",
+    base: "https://api.openweathermap.org/data/2.5/",
+};
+
+const SearchBar = (props) => {
+    const [location, setLocation] = useState("");
+
+    //To get the city name and temperature details and handle the form submition
+    const handlerSubmit = (event) => {
+        event.preventDefault();
+        axios
+            .get(
+                `${api.base}weather?q=${location}&units=metric&APPID=${api.key}`
+            )
+            .then((response) => {
+                const data = [...props.tiles];
+                const cityExists = data.find((x) => x.name === location);
+                if (cityExists === undefined) {
+                    data.push({
+                        id: response.data.id,
+                        name: response.data.name,
+                        temp: response.data.main.temp,
+                    });
+                    props.setTiles(data);
+                } else {
+                    alert("City is already exists.");
+                }
+            })
+            .catch((error) => {
+                console.log(error.message);
+            });
+        console.log({ location });
+        setLocation("");
+    };
+
+    const onChange = (event) => {
+        setLocation(event.target.value);
+    };
+
     return (
         <div className="search-container">
-            <form className="form-container" data-testid="form-wrapper">
+            <form
+                className="form-container"
+                onSubmit={handlerSubmit}
+                data-testid="form-wrapper"
+            >
                 <input
                     type="text"
                     name="searchBar"
                     placeholder="Search for location"
                     className="search-bar"
+                    onChange={onChange}
+                    value={location}
                 />
                 <div className="search-button-container">
                     <button
                         type="submit"
                         className="search-button"
                         data-testid="search-button"
+                        onClick={handlerSubmit}
                     >
                         Search
                     </button>
